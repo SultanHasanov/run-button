3ee1c591a87c63766521be2950d6b07a0c4f6f26
sultan <sultanhasanov987@gmail.com>
2022-06-30 05:35:32 +0300

    Удалил ответ

--- script.js
+++ script.js
@@ -18,8 +18,3 @@ btn_1.addEventListener('mouseenter', () => {
     btn_1.style.left = `${random(0, 90)}%`;
     btn_1.style.top = `${random(0, 90)}%`;
 });
-
-// в случае победы выведем:
-btn.addEventListener('click', () => {
-    alert('Congrats! You hit the button! What are you doing with your life? :D');
-});
